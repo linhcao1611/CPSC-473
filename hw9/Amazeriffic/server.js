a24d6a49ef839d0e5dba33540667a2568d723d2e
linhcao1611 <linhcao@csu.fullerton.edu>
2015-04-29 18:13:33 -0700

    Screenshots

--- hw9/Amazeriffic/server.js
+++ hw9/Amazeriffic/server.js
@@ -32,7 +32,7 @@ io.on('connection', function(socket) { // client connected
 
     socket.on("addToDo", function(content) { // recieve data from client
         socket.broadcast.emit("add", content);
-        console.log(" a ToDo item is added: "content);
+        console.log(" a ToDo item is added");
     });
 });
 
